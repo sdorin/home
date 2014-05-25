128843f3656ec0e8c57da740b57fcc7b86a7623a
Dorin <sarbudorin@gmail.com>
2014-05-25 19:45:50 +0300

    +added gmail.sh

---
+++ conky/.conkyrc/gmail.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+GUSERNAME="your_gmail_username"
+GPASS="your_password"
+
+GOOGLE_ATOM="$(wget --secure-protocol=TLSv1 --timeout=3 -t 1 -q -O - \
+https://${GUSERNAME}:${GPASS}@mail.google.com/mail/feed/atom \
+--http-user="$1" --http-password="$2" --no-check-certificate)"
+EXIT_CODE="$?"
+if [[ $EXIT_CODE != 0 ]];then
+	echo Error!
+	exit 1;
+fi
+MAIL_COUNT="$(echo "$GOOGLE_ATOM"|grep 'fullcount'|sed -e 's/.*<fullcount>\(.*\)<\/fullcount>.*/\1/' 2>/dev/null)"
+if [ -z "$MAIL_COUNT" ]; then
+	echo "0 mesaje noi"
+elif [ "$MAIL_COUNT" == "1" ]; then
+	echo "1 mesaj nou"
+else
+	echo "$MAIL_COUNT mesaje noi"
+fi
